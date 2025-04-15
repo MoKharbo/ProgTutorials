de4ffb9c102861c3faa49c6be7d2d05064290736
Mo <38759@ma-web.nl>
2025-04-15 22:21:14 +0200

    Tutorial07

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     private Rigidbody rb;
-    public float force = 20f;
+    public float force = 15f;
+    
+    public float gravScaleUp = 1f;
+    public float gravScaleDown = 2f;  
+    private float originalGravity = -9.81f;
     public bool onFloor = false;
 
     void Start()
@@ -25,8 +29,20 @@ public class Jump : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && onFloor == true)
         {
-            rb.AddForce(transform.up * force);
+            rb.AddForce(Vector3.up * force, ForceMode.Impulse);
             onFloor = false;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            Physics.gravity = new Vector3(0, gravScaleUp * originalGravity, 0);
+        }
+        else if (rb.velocity.y > 0)
+        {
+            Physics.gravity = new Vector3(0, gravScaleDown * originalGravity, 0);
+        }
+        {
+
         }
     }
 }
