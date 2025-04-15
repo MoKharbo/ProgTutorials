614b3b9a5f659429163783c14596762f705519fd
Mo <38759@ma-web.nl>
2025-04-15 20:42:37 +0200

    Tutorial06

---
+++ Assets/Scripts/TriggerAnimation.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+    public string triggerName;
+    public float delay = 0f;
+    public float resetTime;
+    public KeyCode triggerKey = KeyCode.None;
+
+    public Animator animator;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(triggerKey))
+        {
+            CallTrigger();
+        }
+    }
+    public void CallTrigger()
+    {
+        StartCoroutine(AwaitDelay(delay));
+        StartCoroutine(AwaitReset(resetTime));
+    }
+    private IEnumerator AwaitDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+        animator.SetTrigger(triggerName);
+        if (audioSource != null) audioSource.Play();
+    }
+    private IEnumerator AwaitReset(float time)
+    {
+        yield return new WaitForSeconds(time);
+        animator.ResetTrigger(triggerName);
+    }
+}
